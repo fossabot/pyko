0a30f4133bbe74583f1a8fbb3fa671b9084883ff
mrx04programmer <mjuandavid901@gmail.com>
2022-08-27 18:03:43 -0500

    Test File

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
 from pyko import *
 clear()
+create_zombie_without_port('fls.doubleclick.net', 'facebook.com')
+create_zombie_without_port('fls.doubleclick.net2', 'instagram.com')
+create_zombie_without_port('fls.doubleclick.net3', 'vekatech.com')
+create_zombie_without_port('virtual', '88.203.211.77')
+zombiesAttack('fls.doubleclick.net*', 'facebook.com', 'post')
